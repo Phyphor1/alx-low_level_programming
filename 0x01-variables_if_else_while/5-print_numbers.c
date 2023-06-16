31adabc94c13eeadf339ba98c2dea7f401efdca5
Phyphor1 <fifokolawole95@gmail.com>
2023-06-16 13:31:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	while (n < 10)
+{
+	printf("%d", n);
+	n++;
+}
+printf("\n");
+return (0);
+}
