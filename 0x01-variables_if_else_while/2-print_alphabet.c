dc67f843b4871a14f30cb85705f92a1d3e366615
Phyphor1 <fifokolawole95@gmail.com>
2023-06-16 12:26:58 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char k;
+
+	k = 'a';
+	while
+		(k <= 'z') {
+			putchar(k);
+			k++;
+		}
+	putchar('\n');
+	return (0);
+}
