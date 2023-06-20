96b108912df37a01bd2b510058cca4f8571be6e9
Phyphor1 <fifokolawole95@gmail.com>
2023-06-20 12:50:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - alphabet in lowercase
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	char pa;
+
+	for (pa = 'a'; pa <= 'z'; pa++)
+	_putchar(pa);
+	_putchar('\n');
+}
