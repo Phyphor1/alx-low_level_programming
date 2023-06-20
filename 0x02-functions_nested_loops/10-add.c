4493b2c45173ccecfdcd01a2bbd748b2e7d0f217
Phyphor1 <fifokolawole95@gmail.com>
2023-06-20 15:43:34 -0700

    0-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - addition of two values
+ * @p1: first number
+ * @p2: second number
+ * Return: integer value
+ */
+int add(int p1, int p2)
+{
+	return (p1 + p2);
+}
