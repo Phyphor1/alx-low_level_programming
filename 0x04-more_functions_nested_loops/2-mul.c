6f74b75a50cb0afafdd84a4873689c59d1db0bc8
Phyphor1 <fifokolawole95@gmail.com>
2023-06-22 14:39:42 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - to multiply integers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
