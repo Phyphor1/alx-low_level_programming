34dfe0bfce1cf40d8d2683d0eb9580d32e68cee3
Phyphor1 <fifokolawole95@gmail.com>
2023-06-22 15:30:10 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print 10 times the number from 0 to 14
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m >= 10)
+			_putchar('1');
+			_putchar(m % 10 + '0');
+		}
+			_putchar('\n');
+		}
+}
